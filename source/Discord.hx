fccf1c19622a46c7ffb3e0c655484305a2818002
BushsHaxs <72326501+BushsHaxs@users.noreply.github.com>
2022-02-07 20:36:00 -0600

    fixed discord

--- source/Discord.hx
+++ source/Discord.hx
@@ -8,38 +8,22 @@ class DiscordClient
 {
 	public function new()
 	{
-		trace("Discord Client starting...");
-		DiscordRpc.start({
-			clientID: "863222024192262205",
-			onReady: onReady,
-			onError: onError,
-			onDisconnected: onDisconnected
 		});
 		trace("Discord Client started.");
 
 		while (true)
 		{
-			DiscordRpc.process();
 			sleep(2);
 			//trace("Discord Client Update");
 		}
-
-		DiscordRpc.shutdown();
 	}
 	
 	public static function shutdown()
 	{
-		DiscordRpc.shutdown();
 	}
 	
 	static function onReady()
 	{
-		DiscordRpc.presence({
-			details: "In the Menus",
-			state: null,
-			largeImageKey: 'icon',
-			largeImageText: "Psych Engine"
-		});
 	}
 
 	static function onError(_code:Int, _message:String)
@@ -69,15 +53,6 @@ class DiscordClient
 		{
 			endTimestamp = startTimestamp + endTimestamp;
 		}
-
-		DiscordRpc.presence({
-			details: details,
-			state: state,
-			largeImageKey: 'icon',
-			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
-			smallImageKey : smallImageKey,
-			// Obtained times are in milliseconds so they are divided so Discord can use it
-			startTimestamp : Std.int(startTimestamp / 1000),
             endTimestamp : Std.int(endTimestamp / 1000)
 		});
 
